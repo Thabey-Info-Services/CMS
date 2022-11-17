4114ccef6c57d784f309bb2f0035bda27addbd81
kannabiran.B kanna <killerkanna2019@outlook.com>
2022-11-17 23:02:11 +0530

    Get Detail api Call

---
+++ CMS/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using CMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CMS.Controllers
+{
+    public class AccountController : ApiController
+    {
+        AccountModel[] ObjLogin = new AccountModel[]
+           {
+               new AccountModel{ID=1,Name="Suganya" },
+                new AccountModel{ID=1,Name="Kannabiran" },
+                 new AccountModel{ID=1,Name="Ashok" },
+                  new AccountModel{ID=1,Name="Parkavi" }
+          };
+        // GET api/values
+        [HttpGet]
+        [Route("api/Get/")]
+        public IEnumerable<AccountModel> Get()
+        {
+            return ObjLogin;
+        }
+
+        // GET api/values/5
+        [HttpGet]
+        [Route("api/GetAccount/{id}")]
+        public string GetAccount(int id)
+        {
+            var AccountList = ObjLogin.FirstOrDefault(x => x.ID == id);
+            if (AccountList == null)
+                return NotFound().ToString();
+            else
+                return AccountList.ToString();
+            
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
